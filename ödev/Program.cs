5654dc9d5a55a587a589c0be4f8e8d85cc7ec7cb
Mehmet Kerim Gündemir <gundemirkerim9@gmail.com>
2024-11-21 02:11:56 +0300

    Proje dosyası ekle.

---
+++ ödev/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ödev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi = int.Parse(Console.ReadLine());
+
+            int bolentoplam = 0;
+
+            for (int i = 1; i < sayi; i++)
+            {
+
+                if (sayi % i == 0)
+                {
+                    bolentoplam = bolentoplam + i;
+                }
+               
+
+            }
+            if (bolentoplam == sayi)
+            {
+                Console.WriteLine(" Bu bir mükemmel sayıdır ");
+            }
+            else
+            {
+                Console.WriteLine(" Bu bir mükemmel sayı değildir ");
+            }
+
+
+        }
+    }
+}
+
